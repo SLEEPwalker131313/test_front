e0e3fb9a3a1d2d74f2dd5067c0314a13020eb7e2
Sergey Sukhanov <sergey.n.sukhanov@firstlinesoftware.com>
2017-07-03 15:43:47 +0300

    struct commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by SNSukhanov on 03.07.2017.
+ */
+
+/*
+* gulp - basic plugin
+* gulp-nodemon - for run http server
+* gulp-inject - for inject files into index.html
+* main-bower-files - for build bower files
+*
+*
+* */
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SNSukhanov on 03.07.2017.
+ */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/client/src'));
+
+app.get('/', function (req, res) {
+  res.send(null);
+});
+
+var server = app.listen(1234, function () {
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', 'localhost', port);
+
+});
