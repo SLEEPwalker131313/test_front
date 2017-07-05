1c7916b72fadc7f7aaf1fcde8a8c1f4aa66a9800
Anton Fomin <Anton.Fomin@firstlinesoftware.com>
2017-07-05 11:51:02 +0300

    fixed path in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/client/dist/dev'));
+app.use(express.static(__dirname + '/client/dist/prod'));
 
 app.get('/', function (req, res) {
   res.send(null);
