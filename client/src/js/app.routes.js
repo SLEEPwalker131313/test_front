185a61038d8c9c5b73d5587d7c9ee6b5280baafc
Anton Fomin <Anton.Fomin@firstlinesoftware.com>
2017-07-05 12:20:19 +0300

    app fixed

--- client/src/js/app.routes.js
+++ client/src/js/app.routes.js
@@ -1,27 +1,37 @@
 ;(function (angular) {
-  'use strict';
+    'use strict';
 
-  angular.module('core').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    'ngInject';
+    angular.module('core').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        'ngInject';
 
-    $locationProvider.html5Mode({
-         enabled: true,
-         requireBase: false
-     });
-    $urlRouterProvider.otherwise("/home");
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-    $stateProvider.state('app', {
-      abstract: true,
-      views: {
-        '@': {
-          template: '<app-layout></app-layout>'
-        }
-      }
+        $urlRouterProvider.otherwise("/home");
 
-    }).state('app.auth', {
-      abstract: true
-    }).state('app.non_auth', {
-      abstract: true
+        $stateProvider
+
+            .state('home', {
+                url: '/home',
+                template: '<div>Layout</div>'
+            });
+
+
+        // $stateProvider.state('app', {
+        //     abstract: true,
+        //     views: {
+        //         '@': {
+        //             template: '<app-layout></app-layout>'
+        //         }
+        //     }
+        //
+        // }).state('app.auth', {
+        //     abstract: true
+        //
+        // }).state('app.non_auth', {
+        //     abstract: true
+        // });
     });
-  });
 })(angular);
